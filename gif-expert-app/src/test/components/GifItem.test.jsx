d97481d69d0c3c8826055211ad120e1a60943fab
AlexRose97 <aexlopez@gmail.com>
2023-10-07 11:33:51 -0600

    clase 105-pruebas en helper

--- gif-expert-app/src/test/components/GifItem.test.jsx
+++ gif-expert-app/src/test/components/GifItem.test.jsx
@@ -1,14 +1,30 @@
 
-import {render} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { GifItem } from '../../components';
 
 
 
 describe('Pruebas en GiftItem', () => {
-    const title="Saitama";
-    const url="https://Saitama";
+    const title = "Saitama";
+    const url = "https://saitama/imagen.jpg";
     test('debe de hacer match con el snapshot', () => {
-       const {container} =render(<GifItem title={title} url={url}/>)
-       expect(container).toMatchSnapshot();
+        const { container } = render(<GifItem title={title} url={url} />);
+        expect(container).toMatchSnapshot();
+    });
+
+
+    test('debe mostrar la imagen con la URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+        // screen.debug()
+        // expect(screen.getByRole("img").src).toBe(url);
+        const { src, alt } = screen.getByRole("img");
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+
     });
 });
