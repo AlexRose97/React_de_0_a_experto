d438a00d6f03e41adf557f00bc5d067a126c0209
AlexRose97 <aexlopez@gmail.com>
2023-10-19 16:32:43 -0600

    Jest Function

--- gif-expert-app/src/test/components/AddCategory.test.jsx
+++ gif-expert-app/src/test/components/AddCategory.test.jsx
@@ -13,9 +13,10 @@ describe('Prueba en componente <AddCategory/ >', () => {
 
     test('Debe llamar onNewValue si se ingresa un valor', () => {
         const inputValue = "Saitama";
+        const onNewValue = jest.fn();//simular la funcion
 
         //renderizar la pagina
-        render(<AddCategory onNewValue={() => { }} />);
+        render(<AddCategory onNewValue={onNewValue} />);
 
         //obtener los componentes a manipular
         const input = screen.getByRole("textbox");
@@ -27,6 +28,34 @@ describe('Prueba en componente <AddCategory/ >', () => {
 
         //la pagina al enviar el formulario reinicia el campo de busqueda
         expect(input.value).toBe("")
+
+        //evaluar la funcion que fue llamada
+        expect(onNewValue).toHaveBeenCalled();
+        expect(onNewValue).toHaveBeenCalledTimes(1);//llamada una vez
+        expect(onNewValue).toHaveBeenCalledWith(inputValue);//llamada con el valor enviado
+
         //screen.debug()
+    });
+
+    test('no debe de llamar onNewValue si el input esta vacio', () => {
+        const inputValue = "";
+        const onNewValue = jest.fn();
+
+        //renderizar
+        render(<AddCategory onNewValue={onNewValue} />);
+
+        //obtener valores
+        const input = screen.getByRole("textbox");
+        const form = screen.getByRole("form");
+
+        fireEvent.input(input, { target: { value: inputValue } });
+        fireEvent.submit(form);
+
+        //no se debe haber llamado la funcion
+        expect(onNewValue).toHaveBeenCalledTimes(0);
+        expect(onNewValue).not.toHaveBeenCalled();
+
+
+
     });
 });
