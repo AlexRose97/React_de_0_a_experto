85859e5723722ae2ef8bc92fa8129ca154a21315
AlexRose97 <aexlopez@gmail.com>
2023-11-07 15:33:52 -0600

    preparando ambiente seccion 10

--- 05 - hook-app/src/main.jsx
+++ 05 - hook-app/src/main.jsx
@@ -14,19 +14,21 @@ import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // // <React.StrictMode>
-  //   {/* < HooksApp/> */}
-  //   {/* <CounterApp /> */}
-  //   {/* <CounterWithCustomHook /> */}
-  // <SimpleForm />
-  // <FormWithCustomHook />
-  // <MultipleCustomHooks />
-  // <FocusScreen />
-  // <Layout />
-  // <Memorize />
-  // <MemoHook />
-  // <CallbackHook />
-  <Padre />
-  // </React.StrictMode>,
-)
+import './08-useReducer/intro-reducer'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // // <React.StrictMode>
+//   //   {/* < HooksApp/> */}
+//   //   {/* <CounterApp /> */}
+//   //   {/* <CounterWithCustomHook /> */}
+//   // <SimpleForm />
+//   // <FormWithCustomHook />
+//   // <MultipleCustomHooks />
+//   // <FocusScreen />
+//   // <Layout />
+//   // <Memorize />
+//   // <MemoHook />
+//   // <CallbackHook />
+//   <Padre />
+//   // </React.StrictMode>,
+// )
