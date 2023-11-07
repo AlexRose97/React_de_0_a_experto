1a0c12c226b72d932a903411d3606d0a8ed88037
AlexRose97 <aexlopez@gmail.com>
2023-11-07 17:06:19 -0600

    clase 142

--- 05 - hook-app/src/08-useReducer/intro-reducer.js
+++ 05 - hook-app/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,33 @@
-console.log("hola mundo")
+const initialState = [{
+    id: 1,
+    todo: "Recolectar la piedra del Alma",
+    done: false,
+}];
+
+const toDoReducer = (state = initialState, action = {}) => {
+
+    if (action.type === "[TODO] add toDO") {
+        return [...state, action.payload]
+    }
+
+    return state;
+}
+
+
+
+let toDos = toDoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: "recolectar la piedra del poder",
+    done: false,
+}
+
+const addToDoAction = {
+    type: "[TODO] add toDO",
+    payload: newTodo,
+}
+
+toDos = toDoReducer(toDos, addToDoAction)
+
+console.log({ state: toDos });
