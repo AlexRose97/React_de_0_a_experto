f99798f22b5e9ed4f278ea3e4883f9152d9383a1
AlexRose97 <aexlopez@gmail.com>
2023-11-13 12:39:04 -0600

    clase 176, prueba todoApp

---
+++ 05 - hook-app/tests/08-useReducer/TodoApp.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { TodoApp } from "../../src/08-useReducer/TodoApp";
+import { useTodos } from '../../src/hooks/useTodos';
+
+jest.mock('../../src/hooks/useTodos')
+
+describe('pruebas en <TodoApp/>', () => {
+    useTodos.mockReturnValue({
+        todos: [
+            { id: 1, description: "todo #1", done: false },
+            { id: 2, description: "todo #2", done: true }
+        ],
+        todosCount: 2,
+        pendingTodosCunt: 1,
+        handleNewTodo: jest.fn(),
+        handleDeleteTodo: jest.fn(),
+        handleToggleTodo: jest.fn(),
+    });
+
+
+    test('debe de mostrar el componente correctamente', () => {
+        render(<TodoApp />);
+        expect(screen.getByText("todo #1")).toBeTruthy()
+        expect(screen.getByText("todo #2")).toBeTruthy()
+        expect(screen.getByRole("textbox")).toBeTruthy()
+        // screen.debug()
+    });
+});
