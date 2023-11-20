d04c2633268377561b9ac1d5bd0a563814ff5e94
AlexRose97 <aexlopez@gmail.com>
2023-11-20 10:26:50 -0600

    creacion navigate push y replace

--- 07 - heroes-spa/src/router/AppRouter.jsx
+++ 07 - heroes-spa/src/router/AppRouter.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import { Navbar } from '../ui'
 
 import { LoginPage } from '../auth'
-import { DcPage, MarvelPage } from '../heroes'
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path='marvel' element={<MarvelPage />} />
-                <Route path='dc' element={<DcPage />} />
                 <Route path='login' element={<LoginPage />} />
-                <Route path='/' element={<Navigate to="/marvel" />} />
+                <Route path='/*' element={<HeroesRoutes />} />
             </Routes>
         </>
     )
